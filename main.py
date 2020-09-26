6f3555ecbc3b8e6f02a54b4dfdf6464476bab59c
Weee <meowtit58@Gmail.com>
2020-09-26 18:00:33 +0100

    schema

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from OpenBullet2Python.TestConfig import TestConfig
 from OpenBullet2Python.Models.BotData import BotData
 from OpenBullet2Python.Models.CVar import CVar
 from fastapi import FastAPI,HTTPException
-from fastapi.responses import JSONResponse
 from base64 import b64decode
 from models.config import configIn,configOut
+from fastapi.responses import JSONResponse
 app = FastAPI()
 
 
 @app.post("/")
 async def root():
     return {"message": "Hello World"}
-    
+
 @app.post("/api/import/",response_model=configOut)
 async def test_config(config: configIn):
+    
     # Decode the base64
     try:
-        config.config_text = b64decode(config.config_text).decode('utf-8')
+        config.text = b64decode(config.text).decode('utf-8')
     except:
         raise HTTPException(status_code=400,detail={"message": "Error decoding b64"})
 
@@ -25,7 +26,7 @@ async def test_config(config: configIn):
     data = BotData()
 
     try:
-        TestConfig(config.config_text,data)
+        TestConfig(config.text,data)
     except:
         raise HTTPException(status_code=400,detail={"message": "Error while testing the config"})
 
--- models/config.py
+++ models/config.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel
-from typing import Optional,List
+from pydantic import BaseModel,Field
+from typing import Optional,List,Dict
+
+
+class CVar(BaseModel):
+    name : str = Field(description="The name of the variable", min_length=1)
+    value : str = Field(description="The value of the variable", min_length=1)
+    hidden : bool = Field(False, description="Hidden variables will not be outputted")
 
 class configIn(BaseModel):
-    name: str
-    config_text: str
-    replacement_variables: str
+    name: str = Field(description="Name of the config",min_length=1)
+    text: str = Field(description="Base64 encoded config text",min_length=1)
+    replacement_variables: List[CVar] = Field([], description="Create new variables to be used for replacements")
+
 class configOut(BaseModel):
     name:str
     status: str
