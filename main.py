e9d671808b36dc8463cfda91ecd466a0a8ad54da
Weee <meowtit58@Gmail.com>
2020-09-26 21:34:23 +0100

    Varaiable replacement

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 from OpenBullet2Python.TestConfig import TestConfig
 from OpenBullet2Python.Models.BotData import BotData
 from OpenBullet2Python.Models.CVar import CVar
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,Request
 from base64 import b64decode
 from models.config import configIn,configOut
 from fastapi.responses import JSONResponse
 app = FastAPI()
 
 
+
+class config_exception(Exception):
+    def __init__(self,message:str):
+        self.message = message
+@app.exception_handler(config_exception)
+async def unicorn_exception_handler(request: Request, exc: config_exception):
+    return JSONResponse(
+        status_code=400,
+        content={"message": exc.message},
+    )
 @app.post("/")
 async def root():
     return {"message": "Hello World"}
@@ -20,16 +30,23 @@ async def test_config(config: configIn):
     try:
         config.text = b64decode(config.text).decode('utf-8')
     except:
-        raise HTTPException(status_code=400,detail={"message": "Error decoding b64"})
+        raise config_exception(message="Error decoding b64")
 
     # Data contains the list of variable and the bot status
     data = BotData()
 
+    # Add variables to to the list for repalcement
+    for variable in config.replacement_variables:
+        data.Variables.Set(CVar(variable.name,variable.value,variable.IsCapture,variable.hidden))
+
+
     try:
         TestConfig(config.text,data)
     except:
-        raise HTTPException(status_code=400,detail={"message": "Error while testing the config"})
+        raise config_exception(message="Error while testing the config")
 
-    variables = [{"Name":v.Name,"Value":v.ToString()} for v in data.Variables.All if v.Hidden == False and v.IsCapture == False]
-    capture_variables = [{"Name":v.Name,"Value":v.ToString()} for v in data.Variables.All if v.Hidden == False and v.IsCapture == True]
+    # OpenBullet CVars to pydantic CVars, Values return as a string e.g. Lists, Dicts
+    variables = [{"name":v.Name,"value":v.ToString(),"IsCapture":v.IsCapture,"hidden":v.Hidden} for v in data.Variables.All if v.Hidden == False and v.IsCapture == False]
+    capture_variables = [{"name":v.Name,"value":v.ToString(),"IsCapture":v.IsCapture,"hidden":v.Hidden} for v in data.Variables.All if v.Hidden == False and v.IsCapture == True]
+
     return {"name":config.name,"status":str(data.Status.value),"variables":variables,"capture_variables":capture_variables}
